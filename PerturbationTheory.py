a3d76f50465352b910a1cb35e380259826311164
b3m2a1 <b3m2a1@gmail.com>
2019-07-03 00:07:59 -0700

    Cleaned up structure

--- PerturbationTheory.py
+++ PerturbationTheory.py
@@ -1,78 +1,27 @@
 import numpy as np, scipy.sparse as sp
-from McUtils.Zachary.LazyTensors import SparseTensor
+from Psience import Wavefunction
+from McUtils.Zachary import SparseTensor
 
-class PerturbationTheoryError(Exception):
+__all__ = [
+    'PerturbationTheoryWavefunction',
+    'PerturbationTheoryException',
+    'PerturbationTheoryHamiltonian'
+]
+
+class PerturbationTheoryException(Exception):
     pass
 
-class PerturbationTheoryHandler:
-    """Holds all the data and methods necessary for doing PerturbationTheory
+class PerturbationTheoryWavefunction(Wavefunction):
 
-    """
-    def __init__(self,
-                 coords,
-                 pot_derivs,
-                 modes,
-                 internals
-                 ):
-        """Represents the main handler used in the perturbation theory calculation
+    def __init__(self, hamiltonian):
 
-        :param coords: The current coordinates of the system (used for computing the Jacobians)
-        :type coords:
-        :param pot_derivs: The first, second, third, and fourth derivatives of the potential
-        :type pot_derivs:
-        :param modes: The cartesian coordinate normal modes
-        :type modes: CoordinateSystem
-        :param internals: The internal coordinate normal modes
-        :type internals:
-        """
-        self.coords_n = (len(modes) + 6)
-        self.mode_n = len(modes)
-        self.grad, self.fcs, self.thirds, self.fourths = self._canonicalize_derivs(pot_derivs)
-        self.modes = modes
-        self.internals = internals
+        super().__init__(None, hamiltonian)
 
-    def _canonicalize_derivs(self, derivs):
+    @classmethod
+    def from_params(cls, *params):
+        """Creates a PerturbationTheoryHamiltonian and uses that to get the terms we want"""
 
-        try:
-            grad, fcs, thirds, fourths = derivs
-        except:
-            grad, fcs, derivs = derivs
-            thirds = derivs.third_deriv_array
-            fourths = derivs.fourth_deriv_array
-
-        coord_n = self.coords_n
-        modes_n = self.mode_n
-        if grad.shape != (coord_n,):
-            raise PerturbationTheoryError("{}.{}: length of gradient array ({2[0]}) is not {3[0]}}",
-                                          type(self).__name__,
-                                          "_canonicalize_force_constants",
-                                          grad.shape,
-                                          (coord_n,)
-                                          )
-        if fcs.shape != (coord_n, coord_n):
-            raise PerturbationTheoryError("{}.{}: dimension of force constant array ({2[0]}x{2[1]}) is not {3[0]}x{3[1]}",
-                                          type(self).__name__,
-                                          "_canonicalize_force_constants",
-                                          fcs.shape,
-                                          (coord_n, coord_n)
-                                          )
-        if thirds.shape != (modes_n, coord_n, coord_n):
-            raise PerturbationTheoryError("{}.{}: dimension of third derivative array ({2[0]}x{2[1]}x{2[2]}) is not {3[0]}x{3[1]}x{3[2]}",
-                                          type(self).__name__,
-                                          "_canonicalize_derivs",
-                                          thirds.shape,
-                                          (modes_n, coord_n, coord_n)
-                                          )
-        # this might need to change in the future
-        if fourths.shape != (modes_n, coord_n, coord_n):
-            raise PerturbationTheoryError("{}.{}: dimension of fourth derivative array ({2[0]}x{2[1]}x{2[2]}) is not {3[0]}x{3[1]}x{3[2]}",
-                                          type(self).__name__,
-                                          "_canonicalize_derivs",
-                                          fourths.shape,
-                                          (modes_n, coord_n, coord_n)
-                                          )
-
-        return grad, fcs, thirds, fourths
+        raise NotImplemented
 
     def _perturbtation_wavefunction_coeffs(self, states, energies, expect_1):
         """Computes raw perturbation expansion of the wavefunctions
@@ -135,6 +84,74 @@ class PerturbationTheoryHandler:
         return np.array(en)
 
 class PerturbationTheoryHamiltonian:
+    """Represents the main handler used in the perturbation theory calculation
+
+    :param coords: The current coordinates of the system (used for computing the Jacobians)
+    :type coords:
+    :param pot_derivs: The first, second, third, and fourth derivatives of the potential
+    :type pot_derivs:
+    :param modes: The cartesian coordinate normal modes
+    :type modes: CoordinateSystem
+    :param internals: The internal coordinate normal modes
+    :type internals:
+    """
+
+    def __init__(self,
+                 coords,
+                 pot_derivs,
+                 modes,
+                 internals
+                 ):
+        self.coords = coords
+        self.coords_n = (len(modes) + 6)
+        self.mode_n = len(modes)
+        self.grad, self.fcs, self.thirds, self.fourths = self._canonicalize_derivs(pot_derivs)
+        self.modes = modes
+        self.internals = internals
+
+    def _canonicalize_derivs(self, derivs):
+
+        try:
+            grad, fcs, thirds, fourths = derivs
+        except:
+            grad, fcs, derivs = derivs
+            thirds = derivs.third_deriv_array
+            fourths = derivs.fourth_deriv_array
+
+        coord_n = self.coords_n
+        modes_n = self.mode_n
+        if grad.shape != (coord_n,):
+            raise PerturbationTheoryException("{}.{}: length of gradient array ({2[0]}) is not {3[0]}}",
+                                              type(self).__name__,
+                                          "_canonicalize_force_constants",
+                                              grad.shape,
+                                              (coord_n,)
+                                              )
+        if fcs.shape != (coord_n, coord_n):
+            raise PerturbationTheoryException("{}.{}: dimension of force constant array ({2[0]}x{2[1]}) is not {3[0]}x{3[1]}",
+                                              type(self).__name__,
+                                          "_canonicalize_force_constants",
+                                              fcs.shape,
+                                              (coord_n, coord_n)
+                                              )
+        if thirds.shape != (modes_n, coord_n, coord_n):
+            raise PerturbationTheoryException("{}.{}: dimension of third derivative array ({2[0]}x{2[1]}x{2[2]}) is not {3[0]}x{3[1]}x{3[2]}",
+                                              type(self).__name__,
+                                          "_canonicalize_derivs",
+                                              thirds.shape,
+                                              (modes_n, coord_n, coord_n)
+                                              )
+        # this might need to change in the future
+        if fourths.shape != (modes_n, coord_n, coord_n):
+            raise PerturbationTheoryException("{}.{}: dimension of fourth derivative array ({2[0]}x{2[1]}x{2[2]}) is not {3[0]}x{3[1]}x{3[2]}",
+                                              type(self).__name__,
+                                          "_canonicalize_derivs",
+                                              fourths.shape,
+                                              (modes_n, coord_n, coord_n)
+                                              )
+
+        return grad, fcs, thirds, fourths
+
     def pmatrix_ho(self, n): # we'll depend on the coefficient to be handled later
         """
 
@@ -166,36 +183,59 @@ class PerturbationTheoryHamiltonian:
         ]
         return sp.diags( [ b[0] for b in bands], [ b[1] for b in bands ] )
 
-    def QQQ(self, coefficients, dimensions, qmatrix = None):
+    def _operator_submatrix(self, funcs, dims, totinds, inds):
+        """Returns the operator submatrix for a product operator like piQjpk or whatever
+
+        :param funcs: the functions that take a dimension size and return a matrix for the suboperator
+        :type funcs:
+        :param dims: dimensions of each coordinate (e.g. (5, 8, 2, 9))
+        :type dims: np.ndarray
+        :param totinds: the total list of indices of which inds is a subset
+        :type totinds: np.ndarray
+        :param inds: the list of indices
+        :type inds: np.ndarray
+        :return:
+        :rtype:
+        """
         import functools as ft
 
+        pieces = [ None ] * len(totinds)
+        for f, i in zip(funcs, inds):
+            if pieces[i] is None:
+                pieces[i] = f(dims[i])
+            else:
+                pieces[i] = pieces[i].dot(f(dims[i]))
+
+        for j in np.setdiff1d(totinds, inds):
+            pieces[j] = sp.identity(dims[j])
+
+        return ft.reduce(sp.kron, pieces)
+
+    def product_operator_tensor(self, funcs, dims):
+        """Generates the tensor created from the product of funcs over the dimensions dims
+
+        :param funcs:
+        :type funcs:
+        :param dims:
+        :type dims:
+        :return:
+        :rtype:
+        """
+
+        ndim = (len(dims),)*len(funcs)
+        base_tensor = np.transpose(np.indices(ndim, dtype=object), np.roll(np.arange(len(funcs))))
+        tot_inds = np.arange(len(dims))
+        news_boy = lambda inds, ti=tot_inds, f=funcs, d=dims: self._operator_submatrix(f, d, ti, inds)
+        news_boys = np.apply_along_axis(news_boy, -1, base_tensor)
+
+        return SparseTensor(news_boys)
+
+    def QQQ(self, coefficients, dimensions, qmatrix = None):
+        #TODO: get the coefficients in there...
         if qmatrix is None:
             qmatrix = self.qmatrix_ho
 
-        dimensions = np.asarray(dimensions)
-        M = len(dimensions)
-        tensor = [ [ [ None ] * M ] * M ] * M # initialize the 3D thing
-        for i in range(M):
-            for j in range(M):
-                for k in range(M):
-                    pieces = [ None ] * M
-                    test = {i, j, k}
-                    for n, m in enumerate(dimensions): # we gotta do a 4th loop to build our KP terms
-                        if n in test:
-                            if i == j == k:
-                                pieces[n] = qmatrix(m).dot(qmatrix(m)).dot(qmatrix(m))
-                            elif i == j or i == k or j == k:
-                                pieces[n] = qmatrix(m).dot(qmatrix(m))
-                            elif (i == n == j) or (i == n == k) or (j == n == k):
-                                pieces[n] = qmatrix(m).dot(qmatrix(m))
-                            else:
-                                pieces[n] = qmatrix(m)
-                        else:
-                            pieces[n] = sp.identity(m)
-
-                    tensor[i][j][k] = ft.reduce(sp.kron, pieces)
-
-        return SparseTensor(tensor, (M, M, M))
+        return self.product_operator_tensor((qmatrix, qmatrix, qmatrix), dimensions)
 
     # TODO: gotta actually include the coefficients somehow...?
     def pQp(self,
@@ -205,40 +245,12 @@ class PerturbationTheoryHamiltonian:
                         qmatrix = None
                         ):
 
-        import functools as ft
         if pmatrix is None:
             pmatrix = self.pmatrix_ho
         if qmatrix is None:
             qmatrix = self.qmatrix_ho
 
-        dimensions = np.asarray(dimensions)
-        M = len(dimensions)
-        tensor = [ [ [ None ] * M ] * M ] * M # initialize the 3D thing
-        for i in range(M):
-            for j in range(M):
-                for k in range(M):
-                    pieces = [ None ] * M
-                    test = {i, j, k}
-                    for n, m in enumerate(dimensions): # we gotta do a 4th loop to build our KP terms
-                        if n in test:
-                            if i == j == k:
-                                pieces[n] = pmatrix(m).dot(qmatrix(m)).dot(pmatrix(m))
-                            elif i == k == n:
-                                pieces[n] = pmatrix(m).dot(qmatrix(m))
-                            elif i == j == n:
-                                pieces[n] = pmatrix(m).dot(pmatrix(m))
-                            elif j == k == n:
-                                pieces[n] = qmatrix(m).dot(pmatrix(m))
-                            elif n == k:
-                                pieces[n] = qmatrix(m)
-                            else:
-                                pieces[n] = pmatrix(m)
-                        else:
-                            pieces[n] = sp.identity(m)
-
-                    tensor[i][j][k] = ft.reduce(sp.kron, pieces)
-
-        return SparseTensor(tensor, (M, M, M))
+        return self.product_operator_tensor((pmatrix, qmatrix, pmatrix), dimensions)
 
     def _compute_h1(self, gmatrix_derivs, V_derivs, pQp, QQQ, n, m, dimensions):
         """
@@ -246,7 +258,7 @@ class PerturbationTheoryHamiltonian:
         :param gmatrix_derivs:
         :type gmatrix_derivs: np.ndarray
         :param V_derivs:
-        :type V_derivs:
+        :type V_derivs: np.ndarray
         :param pQp:
         :type pQp:
         :param QQQ:
@@ -262,16 +274,27 @@ class PerturbationTheoryHamiltonian:
         """
         # we'll vectorize the computation of these components in a bit...
 
-        pQp_shape = pQp.shape
+
         dimensions = np.asarray(dimensions)
         bits = np.cumprod(dimensions) # should ignore the last one I think...
         n_i = np.dot(bits, np.asarray(n))
         m_i = np.dot(bits, np.asarray(m))
+
+        pQp_shape = pQp.shape
         piQkpj = np.array([
             pQp[i][j][k][n_i][m_i] for k in pQp_shape[2] for j in pQp_shape[1] for i in pQp_shape[0]
+        ]) # this could be quite slow but the only alternative is to use tons of memory...
+
+        ke = np.dot(gmatrix_derivs.flatten(), piQkpj)
+
+        QQQ_shape = QQQ.shape
+        QiQjQk = np.array([
+            QQQ[i][j][k][n_i][m_i] for k in QQQ_shape[2] for j in QQQ_shape[1] for i in QQQ_shape[0]
         ])
 
-        np.dot(gmatrix_derivs.flatten(), )
+        pe = np.dot(V_derivs.flatten(), QiQjQk)
+
+        return 1/2 * ke + 1/6 * pe
 
 
 
