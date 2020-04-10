e90023c76aab63f4dc1f9a99ae4f7b82e5313c58
b3m2a1 <b3m2a1@gmail.com>
2020-04-10 11:01:29 -0700

    Cartesian version working

--- Tests/__init__.py
+++ Tests/__init__.py
@@ -1,7 +1,2 @@
 
-from .ConverterTests import ConverterTest
-from .CoordinateTests import TestAffineMatrix
-from .FiniteDifferenceTests import FiniteDifferenceTests
-from .GaussianImportTests import GaussianImportTests
-from .DataTests import DataTests
-from ..Peeves.TestUtils import load_tests
+from .VPTTests import *
